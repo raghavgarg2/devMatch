7df7d7ac7f568b853cba183b567bd64769dc2176
Raghav Garg <raghavgarg@Raghavs-MacBook-Air.local>
2025-03-09 00:24:32 +0530

    socket js issue

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,10 +1,10 @@
-import io from "socket.io-client";
+// import io from "socket.io-client";
 
-import { BASE_URL } from "./constants";
+// import { BASE_URL } from "./constants";
 
-export const createSocketConnection = () => {
-  return io(BASE_URL);
-};
+// export const createSocketConnection = () => {
+//   return io(BASE_URL);
+// };
 
 // import io from "socket.io-client";
 // import { BASE_URL } from "./constants";
@@ -16,3 +16,18 @@ export const createSocketConnection = () => {
 //     return io("/", { path: "/api/socket.io" });
 //   }
 // };
+
+import io from "socket.io-client";
+import { BASE_URL } from "./constants";
+
+export const createSocketConnection = () => {
+  if (typeof window !== "undefined" && location.hostname === "localhost") {
+    return io(BASE_URL);
+  } else {
+    return io(BASE_URL, {
+      path: "/socket.io", // Backend ke socket route ka path
+      transports: ["websocket", "polling"], // Transport methods specify karna
+      withCredentials: true, // CORS issue avoid karne ke liye
+    });
+  }
+};
