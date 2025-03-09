f44c085107a929b03ca682f5ee711f22ed1c451c
Raghav Garg <raghavgarg@Raghavs-MacBook-Air.local>
2025-03-09 12:55:19 +0530

    websockets issue

--- server/src/utils/socket.js
+++ server/src/utils/socket.js
@@ -24,6 +24,72 @@
 
 // module.exports = initializeSocket;
 
+// const socket = require("socket.io");
+// const crypto = require("crypto");
+// const { Chat } = require("../models/chat");
+// const ConnectionRequest = require("../models/connectionRequest");
+
+// const getSecretRoomId = (userId, targetUserId) => {
+//   return crypto
+//     .createHash("sha256")
+//     .update([userId, targetUserId].sort().join("$"))
+//     .digest("hex");
+// };
+
+// const initializeSocket = (server) => {
+//   const io = socket(server, {
+//     cors: {
+//       origin: ["http://localhost:5173", "https://dev-match.vercel.app"],
+//       credentials: true,
+//     },
+//   });
+
+//   io.on("connection", (socket) => {
+//     socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+//       const roomId = getSecretRoomId(userId, targetUserId);
+//       // console.log(firstName + " joined Room : " + roomId);
+//       socket.join(roomId);
+//     });
+
+//     socket.on(
+//       "sendMessage",
+//       async ({ firstName, lastName, userId, targetUserId, text }) => {
+//         // Save messages to the database
+//         try {
+//           const roomId = getSecretRoomId(userId, targetUserId);
+//           // console.log(firstName + " " + text);
+
+//           // TODO: Check if userId & targetUserId are friends
+
+//           let chat = await Chat.findOne({
+//             participants: { $all: [userId, targetUserId] },
+//           });
+
+//           if (!chat) {
+//             chat = new Chat({
+//               participants: [userId, targetUserId],
+//               messages: [],
+//             });
+//           }
+
+//           chat.messages.push({
+//             senderId: userId,
+//             text,
+//           });
+
+//           await chat.save();
+//           io.to(roomId).emit("messageReceived", { firstName, lastName, text });
+//         } catch (err) {
+//           console.log(err);
+//         }
+//       }
+//     );
+
+//     socket.on("disconnect", () => {});
+//   });
+// };
+
+// module.exports = initializeSocket;
 const socket = require("socket.io");
 const crypto = require("crypto");
 const { Chat } = require("../models/chat");
@@ -45,21 +111,20 @@ const initializeSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
+    console.log(`🔥 Client connected: ${socket.id}`);
+
     socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
       const roomId = getSecretRoomId(userId, targetUserId);
-      // console.log(firstName + " joined Room : " + roomId);
+      console.log(`📌 ${firstName} joined Room: ${roomId}`);
       socket.join(roomId);
     });
 
     socket.on(
       "sendMessage",
       async ({ firstName, lastName, userId, targetUserId, text }) => {
-        // Save messages to the database
         try {
           const roomId = getSecretRoomId(userId, targetUserId);
-          // console.log(firstName + " " + text);
-
-          // TODO: Check if userId & targetUserId are friends
+          console.log(`📩 ${firstName} sending message in Room: ${roomId}`);
 
           let chat = await Chat.findOne({
             participants: { $all: [userId, targetUserId] },
@@ -78,14 +143,18 @@ const initializeSocket = (server) => {
           });
 
           await chat.save();
+
           io.to(roomId).emit("messageReceived", { firstName, lastName, text });
+          console.log(`✅ Message sent in Room: ${roomId}`);
         } catch (err) {
-          console.log(err);
+          console.error("❌ Error in sendMessage:", err);
         }
       }
     );
 
-    socket.on("disconnect", () => {});
+    socket.on("disconnect", () => {
+      console.log(`❌ Client disconnected: ${socket.id}`);
+    });
   });
 };
 
